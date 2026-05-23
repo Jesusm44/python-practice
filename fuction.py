7f05c1a52701dece74fbf819c7dca9892a4a05ee
Jesus Mejia <jesusmejia.m21@gmail.com>
2026-05-23 03:35:55 -0500

    learning fuction

---
+++ fuction.py
@@ -0,0 +1,119 @@
+# def greet_person(x):
+#     print("Hello",x)
+
+# greet_person("Peter")
+
+
+# def sum_two_numbers(x,y):
+#     return x + y
+
+# def subtract_two_numbers(x,y):
+#     return x - y
+
+# print(sum_two_numbers(5,6))
+
+
+# def give_good_days(name):
+#     print(f'Hello, {name}')
+
+
+# give_good_days("Peter")
+# give_good_days("Mirian")
+
+
+# def personal_data(name, year):
+#     print(f'Hello my name is {name} and I am {year} years old')
+
+# personal_data("Lois", 24)
+# personal_data(24,"lois")
+
+# def area_rectangle(b,h):
+#     return b * h
+
+# print(f'The area of a rectangle is {area_rectangle(5,6)}')
+# print(f'The area of a rectangle is {area_rectangle(5,6,7,8)}') #Error lo pedia el ejercicio.
+
+
+# def data_test(a,b,c):
+
+#     return a / b + c
+
+# print(f'The perimeter is: {data_test(7,15,8)}')
+# print(f'The perimeter is: {data_test(8,15,7)}')
+
+
+# a = input("Enter data: ")
+# b = input("Enter a second piece of information: ")
+# c = input("Enter a third piece of data: ")
+
+# def data_testing(a,b,c):
+#     print(a)
+#     print(b)
+#     print(c)
+
+# data_testing(a,b,c)
+
+
+# def one_data_sum():
+#     return 1 + 2 + 3
+
+# print(one_data_sum())
+
+
+# def two_data_sum(a,b,c):
+#     return a + b + c
+
+# print(two_data_sum(10,20,5))
+
+
+# def sum_two_data(a,b):
+#     return a + b
+
+
+# def sum_two_data_one(a,b):
+#     print(a+b)
+
+
+# result = sum_two_data(5,6)
+# result_one = sum_two_data_one(10,5)
+
+# print(result)
+# print(result_one)
+
+
+# def function_one(a,b,c):
+#     return a + b + c
+
+# print(function_one(5,6,41))
+
+# result = function_one(5,6,41)
+
+# print(f'the perimeter of the scalene triangle: {result}')
+
+
+# def function_with(a, b, c):
+#     return a + b + c
+
+# a = 0
+# b = 0
+# c = 0
+
+# for i in range(3):
+#     data = int(input("enter some data: "))
+   
+
+#     if i == 0:
+#         a = data
+#     elif i == 1:
+#         b = data
+#     else:
+#         c = data
+
+# result = function_with(a,b,c)
+
+# print(result)
+
+    
+
+
+
