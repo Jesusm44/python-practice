7946c8a79933d148d2f640a6f233729a2cf0b5a3
Jesus Mejia <jesusmejia.m21@gmail.com>
2026-08-05 13:22:17 -0500

    day ended

--- function_structured_exercises.py
+++ function_structured_exercises.py
@@ -182,53 +182,200 @@
 #     return total_pay
 
 # while True:
-    # option = input("Enter the options: \n"
-    #     "1. Total to pay, with the discount \n" \
-    #     "2. Exit the app : "
-    # )
+#     option = input("Enter the options: \n"
+#         "1. Total to pay, with the discount \n" \
+#         "2. Exit the app : "
+#     )
 
-    # if option == "1":
-    #     product_price = int(input("Enter the product price: "))
-    #     percentage = int(input("Enter the percentage of the product: "))
-    #     if valid_percentage(percentage):
-    #         print(discounted_products(product_price,percentage))
-    #     else:
-    #         print("ERROR: Invalid percentage")
-    #         break
-    # elif option == "2":
-    #     print("Closing the app...")
-    #     break
-    # else:
-    #     print("ERROR: Invalid option")
-    #     break
-
-estudiantes = [
-    {
-        "id": 1,
-        "nombre": "Ana",
-        "edad": 19,
-        "promedio": 17
-    },
-    {
-        "id": 2,
-        "nombre": "Luis",
-        "edad": 22,
-        "promedio": 14
-    },
-    {
-        "id": 3,
-        "nombre": "María",
-        "edad": 20,
-        "promedio": 19
-    }
-]
+#     if option == "1":
+#         product_price = int(input("Enter the product price: "))
+#         percentage = int(input("Enter the percentage of the product: "))
+#         if valid_percentage(percentage):
+#             print(discounted_products(product_price,percentage))
+#         else:
+#             print("ERROR: Invalid percentage")
+#             break
+#     elif option == "2":
+#         print("Closing the app...")
+#         break
+#     else:
+#         print("ERROR: Invalid option")
+#         break
 
 
+# students = [
+#     {
+#         "id": 1,
+#         "name": "Ana",
+#         "age": 19,
+#         "average": 17
+#     },
+#     {
+#         "id": 2,
+#         "name": "Luis",
+#         "age": 22,
+#         "average": 14
+#     },
+#     {
+#         "id": 3,
+#         "name": "María",
+#         "age": 20,
+#         "average": 19
+#     }
+# ]
 
+# def search_id(students):
+#     search = int(input("Enter the ID you are looking for: "))
 
+#     for student in students:
 
+#         if student["id"] == search:
+#             return student
+ 
+#     return False
 
+# def calculate_general_averages(students):
+#     sum_average = 0
+#     count_general_average = 0
+    
+#     for student in students:
+#         count_general_average += student["average"]
+#         sum_average += 1
 
+#     total_general_average = count_general_average / sum_average
+#     return total_general_average
+
+# def best_average(students):
+#     average_best = None
+
+#     for student in students:
+#         if average_best is None or student["average"] > average_best:
+#             average_best = student["average"]
+#     return average_best
+
+# def worst_average(students):
+#     average_worst = None
+
+#     for student in students:
+#         if average_worst is None or student["average"] < average_worst:
+#             average_worst = student["average"]
+#     return average_worst
+
+# def adults(students):
+#     count_adults = 0
+    
+#     for student in students:
+#         if student["age"] >= 18:
+#             count_adults +=  1
+#     return count_adults 
+
+# def approved_students(students):
+#     count_approved = 0
+
+#     for student in students:
+#         if student["average"] >= 10:
+#             count_approved += 1
+#     return count_approved
+
+# def update_average(students):
+#     student = search_id(students)
+
+#     if student:
+#         change_average = int(input("Enter the average you want to change: "))
+
+#         if change_average > 20 or change_average < 0:
+#             return "ERROR: Average are not allowed"
+#         else:
+#             student["average"] = change_average
+#     else:
+#         return "No student with that ID was found"
+
+# def verify_student(students):
+#     search_student = input("Enter the name of the student you are looking for: ")
+
+#     for student in students:
+#         if student["name"] == search_student:
+#             return f'The student has been found {student["name"]}'
+        
+#     return "Student not fount"
+
+# while True:
+
+#     print("You choose options, to know what you want to do.")
+
+#     options = input("Enter a number to choose the option you  want: \n" \
+#         "1. Search id \n" \
+#         "2. Calculate the overall averages \n"
+#         "3. Find a better average \n"
+#         "4. Find the lowest average \n" 
+#         "5. Count adults \n"
+#         "6. Count students who passed \n"
+#         "7. Update a student's average \n"
+#         "8. Check if a student exists \n"
+#         "9. Exit the app \n" 
+#         ": "
+#     )
+
+#     if options == "1":
+#         print(f'The student found is: {search_id(students)}')
+#     elif options == "2":
+#         print(f'The calculation of averages is: {calculate_general_averages(students)}')
+#     elif options == "3":
+#         print(f'The best average is {best_average(students)}')
+#     elif options == "4":
+#         print(f'The worst average is: {worst_average(students)}')
+#     elif options == "5":
+#         print(f'The number of adults is: {adults(students)}')
+#     elif options == "6":
+#         print(f'Number of students passed: {approved_students(students)}')
+#     elif options == "7":
+#         print(f'Your update was successful: {update_average(students)}')
+#     elif options == "8":
+#         print(f'The verification is correct: {verify_student(students)}')
+#     elif options == "9":
+#         print("Exit the program...")
+#         break
+#     else:
+#         print("ERROR: This option does not exist")
+
+# def obtain_linear_equation_data():
+#     a = int(input("Enter any number: "))
+#     b = int(input("Enter any second number: "))
+#     c = int(input("Enter any third number: "))
+
+#     return a, b, c
+
+# def validate_data_equation_linear():
+    
+#     a,b,c = obtain_linear_equation_data()
+
+#     if a == 0:
+#         if b == c:
+#             print("The equation has infinite solutions (Identity).")
+#         else:
+#             print("The equation has no solution (Inconsistency).")
+#         return None
+    
+#     return a, b, c
+
+# def generation_of_results():
+#     data = validate_data_equation_linear() 
+    
+#     if data is None:
+#         return
+
+#     a, b, c = data
+
+#     result = (c - b) / a
+
+#     return result 
+
+# def show_results():
+#     show = generation_of_results()
+#     if show is not None:
+#         print(f"The result of the function is: {show}")
+
+# show_results()
 
 
 
